60f05e9ccac5aedac940ae4f2e1944a0a796497e
jake mansoor <jakemansoor@oakland.edu>
2025-03-05 09:57:59 -0500

    inputs recieved

--- Mansoor_q11.cpp
+++ Mansoor_q11.cpp
@@ -1,4 +1,22 @@
-/* Filename: IO.cpp
- * Author: 
- * Description:
+/* Filename: Mansoor_q11.cpp
+ * Author: Jake Mansoor
+ * Description:A program that demonstrates IOmusing priinf and scanf, functions and basic algebra in C++
  */
+
+ #include<iostream>
+ using namespace std;
+
+ int main() {
+    double assignnmentGrade;
+    double examGrades;
+    double finalExamGrade;
+
+    printf("Input your grade for your assignemnts: ");
+    scanf("%lf", &assignnmentGrade);
+    printf("Input your grade for your exams combined: ");
+    scanf("%lf", &examGrades);
+    printf("Final Exam grade: ");
+    scanf("%lf", &finalExamGrade);
+
+    
+ }
